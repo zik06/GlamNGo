3eba88b868eb10aa6e1901250c3fa7a797e1c0bc
zil06 <zikrashamim123@gmail.com>
2025-06-13 22:51:29 +0530

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,15 +7,30 @@ import CustomNavbar from './assets/components/CustomNavbar';
 import Carousel_Part from './assets/components/Carousel_Part';
 import Cards_Part from './assets/components/Cards_Part';
 import Footer from './assets/components/Footer';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
- function App() {
+  function HomePage() {
   return (
-    <div>
+    <>
       <CustomNavbar />
       <Carousel_Part />
       <Cards_Part />
       <Footer />
-    </div>
+    </>
+  );
+}
+
+function App() {
+  return (
+    <BrowserRouter>
+      <CustomNavbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        {/* You can add more routes like this */}
+        {/* <Route path="/about" element={<About />} /> */}
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   );
 }
 
